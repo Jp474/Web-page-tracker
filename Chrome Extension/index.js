927123cd6db65bfba45b36933f421c66145356ef
Jp474 <114600797+Jp474@users.noreply.github.com>
2022-12-22 10:28:01 +0100

    Add files via upload

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -1,50 +1,47 @@
-const inputField = document.getElementById("input-txt")
+const inputField = document.getElementById("input-field")
 const inputBtn = document.getElementById("input-btn")
 const tabBtn = document.getElementById("tab-btn")
 const deleteBtn = document.getElementById("delete-btn")
-const ulEl = document.getElementById("ul-el")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-
+const ulEL = document.getElementById("ul-el")
+let leadsFromStorage = JSON.parse(localStorage.getItem("myLeads"))
 let myLeads = []
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
+if (leadsFromStorage) {
+   myLeads = leadsFromStorage
+   render(myLeads)
 }
 
+function render(leads) {
+    
+    let baseString = ``
+    for (let i = 0; i < leads.length; i++) {
+        baseString += `<li><a target="_blank" href="${leads[i]}">${leads[i]}</li>`
+    }  ulEL.innerHTML = baseString
+    return ulEL.innerHTML
+} 
+
+inputBtn.addEventListener("click", function() {
+    if (inputField.value){
+        myLeads.push(inputField.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    inputField.value = ""
+    render(myLeads)
+    }
+    
+})
+
 tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     })
 })
 
-function render(leads) {
-    let baseList = ""
-    for (let i = 0; i < leads.length; i ++) {
-        baseList += `<li> 
-                        <a href="${leads[i]}" target="_blank">${leads[i]}
-                        </a> 
-                     </li>`
-    }
-    ulEl.innerHTML = baseList
-}
-
-deleteBtn.addEventListener("dblclick", function() {
-    inputField.value = ""
-    localStorage.clear()
+deleteBtn.addEventListener("dblclick", function(){
     myLeads = []
+    leadsFromStorage = ""
+    localStorage.clear()
+    inputField.value = ""
     render(myLeads)
-
-}) 
-    
-inputBtn.addEventListener("click", function() {
-    if (inputField.value) {
-        myLeads.push(inputField.value)
-        inputField.value = ""
-        localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        render(myLeads)
-    }
-    
 })
